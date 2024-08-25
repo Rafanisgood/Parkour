44f2edcf86f37254aabe7fccddc1da6492f2b74e
Arthur <150600141+Rafanisgood@users.noreply.github.com>
2024-08-25 21:50:41 +0600

    fixed audio

--- 2d platformer game/game managing/scene_transition.gd
+++ 2d platformer game/game managing/scene_transition.gd
@@ -1,8 +1,11 @@
 extends Node
+	
 
-func _on_lvl_1_pressed():
+
+func _on_button_2_pressed():
 	get_tree().change_scene_to_file("res://scenes/levelz/level1.tscn")
 
-func _on_lvl_2_pressed():
+
+func _on_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/levelz/level2.tscn")
 	
